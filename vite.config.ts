0a6cdf29f3794de95f1bf7b6794f1c28dfed51b9
Lillifee <patrick.matt.85@gmail.com>
2026-06-02 20:35:17 +0200

    feat: migrate CI and development scripts to Vite+ and update configurations

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
   staged: {
-    '*': 'vp check --fix',
+    '*.{js,jsx,ts,tsx}': 'vp check --fix',
   },
   fmt: {
     singleQuote: true,
